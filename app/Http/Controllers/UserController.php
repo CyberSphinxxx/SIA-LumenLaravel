793592deb035e1a4ee58a5a4b4cfacb1a4dc18ae
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-03-03 04:51:38 +0800

    Refractor Code

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,50 +12,34 @@ class UserController extends Controller
 {
     private $request;
 
-    public function __construct(Request $request)
-    {
+    public function __construct(Request $request){
         $this->request = $request;
     }
 
-    /**
-     * Helper method to return a successful JSON response
-     */
-    protected function successResponse($data, $code = Response::HTTP_OK)
-    {
+    /* Helper method to return a successful JSON response */
+    protected function successResponse($data, $code = Response::HTTP_OK){
         return response()->json(['data' => $data], $code);
     }
 
-    /**
-     * Helper method to return an error JSON response
-     */
-    protected function errorResponse($message, $code)
-    {
+    /* Helper method to return an error JSON response */
+    protected function errorResponse($message, $code){
         return response()->json(['error' => $message, 'code' => $code], $code);
     }
 
-    /**
-     * Return the list of users
-     */
-    public function index()
-    {
+    /* Return the list of users */
+    public function index(){
         $users = User::all();
         return $this->successResponse($users);
     }
 
-    /**
-     * Get all users
-     */
-    public function getUsers()
-    {
+    /* Get all users */
+    public function getUsers(){
         $users = User::all();
         return $this->successResponse($users);
     }
 
-    /**
-     * Add a new user
-     */
-    public function add(Request $request)
-    {
+    /*Add a new user*/
+    public function add(Request $request){
         $rules = [
             'username' => 'required|max:20',
             'password' => 'required|max:20',
@@ -67,11 +51,8 @@ class UserController extends Controller
         return $this->successResponse($user, Response::HTTP_CREATED);
     }
 
-    /**
-     * Show details of a single user
-     */
-    public function show($id) 
-    {
+    /*Show details of a single user*/
+    public function show($id){
         $user = User::find($id);
 
         if (!$user) {
@@ -81,11 +62,8 @@ class UserController extends Controller
         return $this->successResponse($user);
     }
 
-    /**
-     * Update an existing user
-     */
-    public function update(Request $request, $id)
-    {
+    /*Update an existing user*/
+    public function update(Request $request, $id){
         $rules = [
             'username' => 'max:20',
             'password' => 'max:20',
@@ -109,9 +87,7 @@ class UserController extends Controller
         return $this->successResponse($user);
     }
 
-    /**
-     * Delete a user
-     */
+    /* Delete a user */
     public function delete($id)
     {
         $user = User::find($id);

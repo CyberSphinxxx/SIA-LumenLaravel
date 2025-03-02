1937da8af35951e6f620c12e899c084bc5b8d924
CyberSphinxxx <johnlemargonzales@gmail.com>
2025-03-03 02:37:41 +0800

    Add gender to the protected fillable

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\Model;
     class User extends Model{
         protected $table = 'tbluser';
         // column sa table
-        protected $fillable = ['username', 'password'];
+        protected $fillable = ['username', 'password','gender'];
  }
